6b3b37955354db4035e6857e87e13309459adf7a
AxelRuizz <78649662+AxelRuizz@users.noreply.github.com>
2024-07-27 19:09:35 -0600

    user management

---
+++ src/main/java/com/mycompany/clases/Customer.java
@@ -0,0 +1,45 @@
+package com.mycompany.clases;
+
+public class Customer {
+    private String name, lastName, email, phoneNumber;
+    
+    public Customer(String name, String lastName, String email, String phoneNumber) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastname) {
+        this.lastName = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
